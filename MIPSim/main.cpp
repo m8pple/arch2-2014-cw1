630dbd3c19b5d5766ba60f37aeb76f14202a6c82
Ollie Ford <me@ojford.com>
2014-10-20 13:56:31 +0100

    Initial Commit

---
+++ MIPSim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MIPSim
+//
+//  Created by Ollie Ford on 20/10/2014.
+//  Copyright (c) 2014 OJFord. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
