58f9d068ea60f21810d71dd898d6afa1c2c11728
Ollie Ford <me@ojford.com>
2014-10-22 22:41:32 +0100

    class shuffle

--- src/ojf13/include/mips_cpu.h
+++ src/ojf13/include/mips_cpu.h
@@ -1,59 +1,18 @@
+//
+//  mips_instr.h
+//  MIPSim
+//
+//  Created by Ollie Ford on 22/10/2014.
+//  Copyright (c) 2014 OJFord. All rights reserved.
+//
 
 #ifndef MIPS_NUM_REG
 #define MIPS_NUM_REG 32U
 #endif
 
-/*
- * Implementation specific classes
- */
-
-struct mips_register{
-public:
-	mips_register(void) : _value(0), _allowSet(true){};
-	mips_register(bool isZero) : _value(0), _allowSet(isZero){};
-	
-	uint32_t value(void) const{
-		return _value;
-	}
-	void value(uint32_t iVal){
-		if(_allowSet)
-			_value = iVal;
-	};
-	
-private:
-	uint32_t _value;
-	bool _allowSet;
-};
-
-/*
- struct mips_zero_reg: mips_register{
- public:
- mips_zero_reg(void) : mips_register(){};
- 
- using mips_register::value;
- void value(uint32_t iVal){
- std::cout << "setting reg 0" << std::endl;
- mips_register::value(0);
- }
- 
- private:
- };*/
-
-struct mips_gp_regs{
-public:
-	mips_gp_regs(void) : _r0(false){};
-	
-	mips_register& operator[](int idx){
-		if(idx >= MIPS_NUM_REG)
-			throw mips_ErrorInvalidArgument;
-		else
-			return idx==0 ? _r0 : _r[idx-1];
-	}
-	
-private:
-	mips_register _r0;
-	mips_register _r[MIPS_NUM_REG-1];
-};
+struct mips_register;
+struct mips_gp_regs;
+struct mips_cpu_impl;
 
 typedef enum _mips_cpu_stage{
 	IF	=0,
@@ -62,43 +21,3 @@ typedef enum _mips_cpu_stage{
 	MEM	=3,
 	WB	=4
 } mips_cpu_stage;
-
-struct mips_cpu_impl{
-public:
-	mips_cpu_impl(void) : r(), _pc(), _stage(IF){};
-	
-	mips_error reset(void){
-		
-		//Zero registers
-		for(int i=0; i<MIPS_NUM_REG; ++i){
-			r[i].value(0);
-		}
-		
-		_pc.value(0);
-		_hi.value(0);
-		_lo.value(0);
-		
-		return mips_Success;
-	}
-	
-	void step(void){
-		_pc.value(_pc.value()+4);
-	}
-	
-	void pc(uint32_t iVal){
-		_pc = iVal;
-	}
-	
-	uint32_t pc(void) const{
-		return _pc.value();
-	}
-	
-	mips_gp_regs	r;
-	
-private:
-	mips_register	_pc;
-	mips_register	_hi;
-	mips_register	_lo;
-	mips_cpu_stage	_stage;
-};
-
--- src/ojf13/mips_cpu.cpp
+++ src/ojf13/mips_cpu.cpp
@@ -1,7 +1,120 @@
+//
+//  mips_instr.h
+//  MIPSim
+//
+//  Created by Ollie Ford on 22/10/2014.
+//  Copyright (c) 2014 OJFord. All rights reserved.
+//
+
 #include "../../include/mips.h"
 #include "include/mips_cpu.h"
 
 #include <iostream>
+
+struct mips_register{
+public:
+	mips_register(void) : _value(0), _allowSet(true){};
+	mips_register(bool isZero) : _value(0), _allowSet(isZero){};
+	
+	uint32_t value(void) const{
+		return _value;
+	}
+	void value(uint32_t iVal){
+		if(_allowSet)
+			_value = iVal;
+	};
+	
+protected:
+	uint32_t _value;
+	
+private:
+	bool _allowSet;
+};
+
+struct mips_gp_regs{
+public:
+	mips_gp_regs(void) : _r0(false){};
+	
+	mips_register& operator[](int idx){
+		if(idx >= MIPS_NUM_REG)
+			throw mips_ErrorInvalidArgument;
+		else
+			return idx==0 ? _r0 : _r[idx-1];
+	}
+	
+private:
+	mips_register _r0;
+	mips_register _r[MIPS_NUM_REG-1];
+};
+
+struct mips_reg_sp: mips_register{
+public:
+	mips_reg_sp(void) : mips_register(false){};
+	
+	using mips_register::value;
+	//make sure we know "we are the CPU" when setting
+	void value(uint32_t){
+		throw mips_ExceptionAccessViolation;
+	}
+	void internal_set(uint32_t iVal){
+		_value = iVal;
+	}
+	
+private:
+};
+
+struct mips_reg_pc: mips_reg_sp{
+public:
+	mips_reg_pc(void) : mips_reg_sp(){};
+	
+	void advance(void){
+		internal_set(value()+4);
+	}
+	
+private:
+};
+
+struct mips_cpu_impl{
+public:
+	mips_cpu_impl(void) : r(), _pc(), _stage(IF){};
+	
+	mips_error reset(void){
+		
+		//Zero registers
+		for(int i=0; i<MIPS_NUM_REG; ++i){
+			r[i].value(0);
+		}
+		
+		_pc.internal_set(0);
+		_hi.internal_set(0);
+		_lo.internal_set(0);
+		
+		return mips_Success;
+	}
+	
+	void step(void){
+		_pc.internal_set(_pc.value()+4);
+		_pc.advance();
+	}
+	
+	//make sure we know "we are the CPU" when setting
+	void internal_pc_set(uint32_t iVal){
+		_pc.internal_set(iVal);
+	}
+	
+	uint32_t pc(void) const{
+		return _pc.value();
+	}
+	
+	mips_gp_regs	r;
+	
+private:
+	mips_reg_pc		_pc;
+	mips_reg_sp		_hi;
+	mips_reg_sp		_lo;
+	mips_cpu_stage	_stage;
+};
+
 
 mips_cpu_h mips_cpu_create(mips_mem_h mem){
     return new mips_cpu_impl();
@@ -51,6 +164,6 @@ mips_error mips_cpu_get_pc(mips_cpu_h cpu, uint32_t *oVal){
 }
 
 mips_error mips_cpu_set_pc(mips_cpu_h cpu, uint32_t iVal){
-	cpu->pc(iVal);
+	cpu->internal_pc_set(iVal);
 	return mips_Success;
 }
