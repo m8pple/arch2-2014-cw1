7fda029c5306bd800c66317e506fd01289c030fd
Ollie Ford <me@ojford.com>
2014-10-25 19:29:19 +0100

    introduced generic I/R type instruction wrappers for easier testing

--- src/ojf13/include/test_mips.h
+++ src/ojf13/include/test_mips.h
@@ -10,23 +10,39 @@
 #define MIPS_NUM_REG	32U
 #endif
 
-#define NUM_TESTS		04U
+#define NUM_TESTS		06U
+
+#if DEBUG==1
+#include "mips_instr.h"
+#else
+#include "include/mips_instr.h"
+#endif
 
 struct testResult{
     const char* instr;
     const char* desc;
     int passed;
 };
+
 void runTest(testResult(*test)(mips_cpu_h, mips_mem_h), mips_cpu_h, mips_mem_h);
+
+typedef int (*verifyFuncI)(uint32_t, uint32_t);
+typedef int (*verifyFuncR)(uint32_t, uint32_t, uint32_t);
+testResult RTypeResult(mips_cpu_h, mips_mem_h, mips_asm, verifyFuncR);
+testResult ITypeResult(mips_cpu_h, mips_mem_h, mips_asm, verifyFuncI);
 
 testResult registerReset(mips_cpu_h, mips_mem_h);
 testResult memoryIO(mips_cpu_h, mips_mem_h);
+testResult ADDResult(mips_cpu_h, mips_mem_h);
+testResult ADDIResult(mips_cpu_h, mips_mem_h);
 testResult ANDResult(mips_cpu_h, mips_mem_h);
 testResult ANDInputs(mips_cpu_h, mips_mem_h);
 
 testResult (*tests[NUM_TESTS])(mips_cpu_h, mips_mem_h) = {
 	registerReset,
 	memoryIO,
+	ADDResult,
+	ADDIResult,
 	ANDResult,
 	ANDInputs
 };
--- src/ojf13/test_mips.cpp
+++ src/ojf13/test_mips.cpp
@@ -8,14 +8,15 @@
 
 #include "mips_test.h"
 #include "include/test_mips.h"
-#include "include/mips_instr.h"
 
 #include <random>
+#include <string>
 
 int main(){
     mips_mem_h mem = mips_mem_create_ram(1<<5, 4);
     mips_cpu_h cpu = mips_cpu_create(mem);
-    
+	
+	srand((unsigned)time(NULL));
     mips_test_begin_suite();
 
 	for(int i=0; i<NUM_TESTS; ++i)
@@ -61,53 +62,133 @@ testResult registerReset(mips_cpu_h cpu, mips_mem_h mem){
 
 testResult memoryIO(mips_cpu_h cpu, mips_mem_h mem){
 	int correct = 1;
-	srand(764835875);
 	
-	for(int r=0; r<1024; r++){
-		uint8_t ibuf[4] = {
-			(uint8_t)(rand()%(1<<31)),
-			(uint8_t)(rand()%(1<<31)),
-			(uint8_t)(rand()%(1<<31)),
-			(uint8_t)(rand()%(1<<31))
-		};
-		mips_mem_write(mem, 0x0, 4, ibuf);
-		
-		uint8_t obuf[4];
-		mips_mem_read(mem, 0x0, 4, obuf);
-		
-		for(int i=0; i<4; ++i)
-			correct &= obuf[i]==ibuf[i] ? 1 : 0;
+	try{
+		for(int r=0; r<1024 && correct; ++r){
+			uint8_t ibuf[4] = {
+				(uint8_t)(rand()%(1<<31)),
+				(uint8_t)(rand()%(1<<31)),
+				(uint8_t)(rand()%(1<<31)),
+				(uint8_t)(rand()%(1<<31))
+			};
+			mips_mem_write(mem, 0x0, 4, ibuf);
+			
+			uint8_t obuf[4];
+			mips_mem_read(mem, 0x0, 4, obuf);
+			
+			for(int i=0; i<4; ++i)
+				correct &= obuf[i]==ibuf[i] ? 1 : 0;
+		}
+	} catch(mips_error) {
+		correct = 0;
 	}
-	
 	return {"<INTERNAL>", "Check can read same value back after write.", correct};
 }
 
+testResult RTypeResult(mips_cpu_h cpu, mips_mem_h mem, mips_asm mnemonic, verifyFuncR verfunc){
+	int correct = 1;
+	
+	try{
+		for(int r=0; r<1024 && correct; ++r){
+			uint32_t shift = rand();
+			uint32_t r1 = rand();
+			uint32_t r2 = rand();
+			
+			mips_mem_write(mem, 0x0, 4, Instruction(mnemonic, 1, 2, 3, shift).bufferedVal());
+			
+			mips_cpu_set_register(cpu, 1, r1);
+			mips_cpu_set_register(cpu, 2, r2);
+			
+			mips_cpu_set_pc(cpu, 0);
+			mips_cpu_step(cpu);
+			
+			uint32_t r3;
+			mips_cpu_get_register(cpu, 3, &r3);
+			correct = r3 == verfunc(r1, r2, shift);
+		}
+	} catch(mips_error) {
+		correct = 0;
+	}
+	std::string desc ="Check result of ";
+	desc += mipsInstruction[mnemonic].mnem;
+	desc += "-ing.";
+	return {mipsInstruction[mnemonic].mnem, desc.c_str(), correct};
+}
+
+testResult ITypeResult(mips_cpu_h cpu, mips_mem_h mem, mips_asm mnemonic, verifyFuncI verfunc){
+	int correct = 1;
+	
+	try{
+		for(int r=0; r<1024 && correct; ++r){
+			uint32_t r1 = rand();
+			uint32_t imm= rand();
+			
+			mips_mem_write(mem, 0x0, 4, Instruction(mnemonic, 1, 2, imm).bufferedVal());
+			
+			mips_cpu_set_register(cpu, 1, r1);
+			
+			mips_cpu_set_pc(cpu, 0);
+			mips_cpu_step(cpu);
+			
+			uint32_t r2;
+			mips_cpu_get_register(cpu, 2, &r2);
+			correct = r2 == verfunc(r1, imm);
+		}
+	} catch(mips_error) {
+		correct = 0;
+	}
+	std::string desc ="Check result of ";
+	desc += mipsInstruction[mnemonic].mnem;
+	desc += "-ing.";
+	return {mipsInstruction[mnemonic].mnem, desc.c_str(), correct};
+}
+
+uint32_t ADDverify(uint32_t r1, uint32_t r2, uint32_t shift){
+	return r1+(r2<<shift);
+}
+testResult ADDResult(mips_cpu_h cpu, mips_mem_h mem){
+	return RTypeResult(cpu, mem, ADD, (verifyFuncR)ADDverify);
+}
+
+uint32_t ADDIverify(uint32_t r1, uint32_t i){
+	return r1+i;
+}
+testResult ADDIResult(mips_cpu_h cpu, mips_mem_h mem){
+	return ITypeResult(cpu, mem, ADDI, (verifyFuncI)ADDIverify);
+}
+
+uint32_t ANDverify(uint32_t r1, uint32_t r2, uint32_t shift){
+	return r1&(r2<<shift);
+}
 testResult ANDResult(mips_cpu_h cpu, mips_mem_h mem){
-	mips_cpu_set_register(cpu, 1, 0x00FF00F0);
-	mips_cpu_set_register(cpu, 2, 0x000F0F0F);
-	mips_mem_write(mem, 0x0, 4, Instruction(AND, 1, 2, 3, 0).bufferedVal());
-	
-	mips_cpu_set_pc(cpu, 0);
-	mips_cpu_step(cpu);
-	
-	uint32_t r3;
-	mips_cpu_get_register(cpu, 3, &r3);
-	int correct = r3==0x000F0000;
-    return {"AND", "Check result of AND-ing.", correct};
+	return RTypeResult(cpu, mem, AND, (verifyFuncR)ANDverify);
 }
 
 testResult ANDInputs(mips_cpu_h cpu, mips_mem_h mem){
-	mips_cpu_set_register(cpu, 1, 0x00FF00F0);
-	mips_cpu_set_register(cpu, 2, 0x000F0F0F);
+	int correct = 1;
 	mips_mem_write(mem, 0x0, 4, Instruction(AND, 1, 2, 3, 0).bufferedVal());
 	
-	mips_cpu_set_pc(cpu, 0);
-	mips_cpu_step(cpu);
-	
-	uint32_t r1,r2;
-	mips_cpu_get_register(cpu, 1, &r1);
-	mips_cpu_get_register(cpu, 2, &r2);
-	int correct = 1 && (r1==0x00FF00F0) && (r2==0x000F0F0F);
+	try{
+		for(int r=0; r<1024 && correct; ++r){
+			uint32_t ir1 = rand();
+			uint32_t ir2 = rand();
+			mips_cpu_set_register(cpu, 1, ir1);
+			mips_cpu_set_register(cpu, 2, ir2);
+			
+			mips_cpu_set_pc(cpu, 0);
+			mips_cpu_step(cpu);
+		
+			mips_cpu_set_pc(cpu, 0);
+			mips_cpu_step(cpu);
+		
+			uint32_t or1,or2;
+			mips_cpu_get_register(cpu, 1, &or1);
+			mips_cpu_get_register(cpu, 2, &or2);
+			correct = (or1==ir1) && (or2==ir2);
+		}
+	} catch(mips_error){
+		correct = 0;
+	}
 	return {"AND", "Check input registers unchanged after AND-ing.", correct};
 }
 
